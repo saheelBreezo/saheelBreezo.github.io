8b3418fec665d8eafbc369149471fbe019368043
saheelBreezo <saheel@blueskyhq.io>
2024-04-17 00:18:20 +0530

    - updated tsconfig

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteImage: `/banner.jpg`,
+    siteTitle: `Saheel Ahmed`,
+    siteTitleAlt: `Saheel Ahmed`,
+    siteHeadline: `Saheel Ahmed`,
+    siteUrl: `https://saheelBreezo.github.io`,
+    siteDescription: `Saheel Ahmed Blog`,
+    // siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@saheelBreezo`,
   },
   trailingSlash: `always`,
   plugins: [
@@ -36,11 +36,11 @@ const config: GatsbyConfig = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/saheelahmed04`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Github`,
+            url: `https://github/saheelBreezo`,
           },
         ],
       },
@@ -123,7 +123,7 @@ const config: GatsbyConfig = {
   }
 }`,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Saheel Ahmed's Blog`,
           },
         ],
       },
